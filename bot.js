a6e901e2bc18b3a6083620340a79cbdbc1ac7c30
FireWaran <firewaran@gmail.com>
2017-11-05 12:36:09 +0100

    basic command structure

--- bot.js
+++ bot.js
@@ -6,10 +6,46 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+	var msgParts = message.split(" ");	
+	
+	if (msgParts[0] === 'bot') {
+		switch (msgParts[1]) {
+		case "ping":
+			message.channel.send('pong');
+			break;
+		case "userConfig"
+			break;
+		case "credits"
+			break;
+		case "checkTypes"
+			break;
+		case "nativeStatus"
+			break;
+		case "info"
+			break;
+		case "config"
+			break;
+		case "total"
+			break;
+		case "users"
+			break;		
+		case "shutdown"
+			break;
+		case "update"
+			break;		
+		case "clear"
+			break;
+		case "check"
+			break;
+		case "status"
+			break;
+		case "help"
+			break;
+		default:
+			message.channel.send('Sorry dude, i don\'t understand "' + msgParts[1] + '"');		
+		}
+	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+// Login and Run the bot.
 client.login(process.env.BOT_TOKEN);
